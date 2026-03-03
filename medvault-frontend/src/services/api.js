0f7e36bf44da6c86e8178db06f87516507b7719a
sujit-prog <148777047+sujit-prog@users.noreply.github.com>
2026-03-03 23:23:39 +0530

    deploy added

--- medvault-frontend/src/services/api.js
+++ medvault-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://medivault-lp09.onrender.com/api",
 });
 
 // Attach JWT automatically
